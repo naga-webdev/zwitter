77bdb954fc2c302d381e24af69b351f48c1e74c6
successnaga <naga2054@gmail.com>
2017-05-19 18:39:39 +0530

    init zwitter

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bp = require('body-parser');
+
+var app = express();
+app.use(bp.json());
+
+app.set('port',( process.env.PORT || 5001 ))
+
+app.use(express.static(__dirname +'/dist'));
+
+
+app.get('/',function(req,res){
+    res.send();
+})
+
+app.listen(app.get('port') ,function(){
+    console.log('server is running at '+ app.get('port'));
+})
