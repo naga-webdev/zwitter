4cb2266ccbd4cad199b6136ac1a80205612d0c56
successnaga <naga2054@gmail.com>
2017-05-22 11:41:14 +0530

    server.js routing

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/',function(req,res){
     res.send();
 })
 
+
+app.get('*',function(req,res){
+    return res.redirect('/');
+})
+
+
 app.listen(app.get('port') ,function(){
     console.log('server is running at '+ app.get('port'));
 })
